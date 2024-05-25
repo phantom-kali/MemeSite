d845d7507c44629a56f6cb5626c27b1210c9e6b1
Fidel <72069127+phantom-kali@users.noreply.github.com>
2024-05-25 19:50:29 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://fidel:GreatP0wer@localhost:5432/meme_platform'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://your_credentials'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'app/uploads'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
